5e5d27733b61f81971254b58b6c194f014f6fac1
Anders Hoff <inconvergent@gmail.com>
2019-09-23 21:27:26 +0200

    some more tweaks (add @)

--- dat/parse.sh
+++ dat/parse.sh
@@ -1,17 +1,16 @@
 #/bin/bash
 
-gridfont parse original.json original --svg && \
-gridfont write original/res.json original/img.svg \
-"abcdefghijklmnopqr
+set -e
+
+TXT="abcdefghijklmnopqr
 stuvwxyz1234567890
-*+()/\[]^=-\!.,<>'#|_%?~\"
+*+()/\[]^=-\!.,<>'#|_@%?~\"
 the quick (brown) fox
 jumps over the lazy dog\!"
 
-gridfont parse smooth.json smooth --svg && \
-gridfont write smooth/res.json smooth/img.svg \
-"abcdefghijklmnopqr
-stuvwxyz1234567890
-*+()/\[]^=-\!.,<>'#|_%?~\"
-the quick (brown) fox
-jumps over the lazy dog\!"
+gridfont parse original.json original --svg
+gridfont write original/res.json original/img.svg "$TXT"
+
+gridfont parse smooth.json smooth --svg
+gridfont write smooth/res.json smooth/img.svg "$TXT"
+
